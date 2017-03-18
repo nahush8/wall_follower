80a05016e889e7b26bd84e8719dc586412416d1a
nahushg <nahushg@vt.edu>
2017-03-18 17:26:42 -0400

    Naive wall repel control

--- scripts/cmd_vel.py
+++ scripts/cmd_vel.py
@@ -6,7 +6,7 @@ from mavros.utils import *
 from mavros_msgs.msg import State
 from mavros_msgs.srv import SetMode, StreamRate, StreamRateRequest, CommandBool
 from geometry_msgs.msg import *
-from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Joy, LaserScan
 import time
 from kb_hit import KBHit
 
@@ -89,14 +89,15 @@ def flight():
         lv = joy.axes[1] #Z  
         rh = joy.axes[2] #x
         rv = joy.axes[3] #y
-
+        
         if lh != 0.0 or lv != 0.0 or rh !=0.0 or rv !=0.0:
             vel.twist.linear.x = rh * LINEAR_X_MUL_FACTOR
             vel.twist.linear.y = -rv * LINEAR_Y_MUL_FACTOR
             vel.twist.linear.z = lv * LINEAR_Z_MUL_FACTOR
             vel.twist.angular.z = lh * ANGULAR_Z_MUL_FACTOR
             local_vel_pub.publish(vel)
-
+            curr_vel_x = vel.twist.linear.x
+            curr_vel_y = vel.twist.linear.y
         else:
             '''
             vel.twist.linear.x = 0
@@ -108,6 +109,16 @@ def flight():
             curr_pose = rospy.wait_for_message("/mavros/local_position/pose" ,PoseStamped)
             local_pos_pub.publish(curr_pose)
 
+        '''
+        A basic wall repel control
+        '''
+        laserData = rospy.wait_for_message("/laser/scan", LaserScan)
+        if any(value < 2 and value > 1 for value in laserData.ranges):
+            vel.twist.linear.x = -curr_vel_x * 2
+            vel.twist.linear.y = -curr_vel_y * 2
+            vel.twist.linear.z = lv * LINEAR_Z_MUL_FACTOR
+            vel.twist.angular.z = lh * ANGULAR_Z_MUL_FACTOR
+            local_vel_pub.publish(vel)
         #print curr_pose
 
     #rospy.spin()
