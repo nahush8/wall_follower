d338bed0f5320a1fbcda20ef562177811ff37c20
nahushg <nahushg@vt.edu>
2017-05-01 16:39:48 -0400

    Good fit kernel param

--- scripts/gp.py
+++ scripts/gp.py
@@ -20,18 +20,24 @@ class update_gp_class:
 		tempList = []
 		for elements in recordTraining:
 			val =  min(elements[0])
+			if val <= 7:
 			#print "----------"
-			trainingX.append(val)
+				trainingX.append(val)
 			#trainingX.append(elements[0])
-			targetX.append(elements[1])
+				targetX.append(elements[1])
 		print "IN UPDATE GP"
+		
+
 		binsHist = np.arange(0,6,0.01)
 		plt.figure(1)
-		plt.subplot(211)
+		plt.subplot(311)
 		plt.xlabel('Minimum Distance to obstacle')
 		plt.ylabel('Number of Samples')
 		plt.hist(trainingX,bins=binsHist,color='r')
-		
+		plt.grid()
+		plt.subplot(312)
+		plt.scatter(trainingX,targetX,color='red')
+		plt.grid()
 		DX , tX = np.matrix(trainingX), np.matrix(targetX)	
 		#For 2D case
 		#DX , tX = np.array(trainingX), np.array(targetX)	
@@ -39,14 +45,13 @@ class update_gp_class:
 		varList = []
 		print DX.shape
 		print tX.shape
+		kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-1, 1e1))
 		
-		kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-		
-		gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9,alpha=1e-2)	
+		gp = GaussianProcessRegressor(kernel=kernel,optimizer='fmin_l_bfgs_b' ,n_restarts_optimizer=9,alpha=1e-2)	
 
 		print "DOING GP FIT"
 		gp.fit(DX.transpose(), tX.transpose())
-		
+		print gp.get_params()
 		#For 2D case
 		#gp.fit(DX, tX) 
 		
@@ -54,7 +59,7 @@ class update_gp_class:
 		
 		testX = []
 		testTargetX = []
-		testRecord = np.arange(0,6,0.01)
+		testRecord = np.arange(0,6,0.001)
 		for elements in testRecord:
 			testX.append(elements)
 
@@ -77,23 +82,22 @@ class update_gp_class:
 			errorList.append(mu[0])
 			varList.append(sigma[0])
 			i+=1	
-		plt.subplot(212)
+		plt.subplot(313)
 		for i in range(0,len(errorList)):
 
 			if testX[i] < 50:
-				#plt.scatter(testX[i],errorList[i])
+				#plt.scatter(test[i],errorList[i])
 				plt.errorbar(testX[i], errorList[i], varList[i], linestyle='None', marker='^',ecolor='g')
-		
 		plt.xlim(0,7)
 		plt.xlabel('Minimum Distance to obstacle')
 		plt.ylabel('Predicted reward')
 		
-		iterator = np.arange(0,6,0.01)
+		iterator = np.arange(0,6,0.001)
 		for i in iterator:
 			if i > 3 and i < 4:
 				plt.plot(i,1,'.b')
 			else:
 				plt.plot(i,-1,'.b')
-
+		plt.grid()
 		plt.show()
 		
